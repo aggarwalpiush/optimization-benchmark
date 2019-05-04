96ebbd1c2e2ff1242fbc1f885c4f319d356e96ed
Piush Aggarwal <aggarwalpiush@Piushs-MacBook-Pro.local>
2019-05-04 16:57:00 +0200

    push back travis to github

--- .travis/push.sh
+++ .travis/push.sh
@@ -11,11 +11,11 @@ commit_website_files() {
   git commit --message "Travis build: 1"
 }
 
-#upload_files() {
- # git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  #git push --quiet --set-upstream origin-pages gh-pages 
-#}
+upload_files() {
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/aggarwalpiush/optimization-benchmark.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
 
 setup_git
 commit_website_files
-#upload_files
+upload_files
