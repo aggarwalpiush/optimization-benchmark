5f8e1cf1aaaf5d8eecab4c5523129ee6a135e805
Piush Aggarwal <aggarwalpiush@Piushs-MacBook-Pro.local>
2019-05-04 17:05:29 +0200

    push back travis to github

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,18 +2,25 @@
 
 setup_git() {
   git config --global user.email "aggarwalpiush@travis-ci.org"
+  echo §?
   git config --global user.name "aggarwalpiush"
+  echo §?
 }
 
 commit_website_files() {
   git checkout -b gh-pages
+  echo §?
   git add . *.png
+  echo §?
   git commit --message "Travis build: 1"
+  echo §?
 }
 
 upload_files() {
-  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/aggarwalpiush/optimization-benchmark.git > /dev/null 2>&1
+  git remote add origin-pages https://"${GITHUB_TOKEN}"@github.com/aggarwalpiush/optimization-benchmark.git > /dev/null 2>&1
+  echo §?
   git push --quiet --set-upstream origin-pages gh-pages 
+  echo §?
 }
 
 setup_git
