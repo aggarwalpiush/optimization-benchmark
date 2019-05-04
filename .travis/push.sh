4b9e10acc1d568ea7dbc2433b2a019267687ef75
Piush Aggarwal <aggarwalpiush@Piushs-MacBook-Pro.local>
2019-05-04 17:16:18 +0200

    push back travis to github

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,28 +1,8 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "aggarwalpiush@travis-ci.org"
-  echo §?
-  git config --global user.name "aggarwalpiush"
-  echo §?
-}
 
-commit_website_files() {
-  git checkout -b gh-pages
-  echo §?
-  git add . *.png
-  echo §?
-  git commit --message "Travis build: 1"
-  echo §?
-}
-
-upload_files() {
-  git remote add origin-pages https://"${GITHUB_TOKEN}"@github.com/aggarwalpiush/optimization-benchmark.git > /dev/null 2>&1
-  echo §?
-  git push --quiet --set-upstream origin-pages gh-pages 
-  echo §?
-}
-
-setup_git
-commit_website_files
-upload_files
+git checkout master
+git add *.png
+git remote add origin2 https://${GITHUB_TOKEN}@github.com/aggarwalpiush/optimization-benchmark.git
+git commit -m "update leaderboard"
+git push origin2
